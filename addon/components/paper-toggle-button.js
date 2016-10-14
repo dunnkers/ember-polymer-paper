0294be8b4e707baf73e21b96fcbd314d861a748f
Dunnkers <dunnkers@hotmail.com>
2016-10-14 17:21:15 +0200

    Add paper-toggle-button component. ✅

---
+++ addon/components/paper-toggle-button.js
@@ -0,0 +1,7 @@
+import PaperCheckbox from './paper-checkbox';
+import layout from '../templates/components/paper-toggle-button';
+
+export default PaperCheckbox.extend({
+  layout,
+  tagName: 'paper-toggle-button'
+});
