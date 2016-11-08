b1d73fd4003d1eb55f4fed6c41bb69d0c17e54f4
Dunnkers <dunnkers@hotmail.com>
2016-11-08 17:38:53 +0100

    Only register actual snippets, not helper-snippets. 🔨

--- tests/dummy/app/initializers/application.js
+++ tests/dummy/app/initializers/application.js
@@ -1,18 +1,26 @@
 import Ember from 'ember';
-const { Controller } = Ember;
+const { Controller, Service } = Ember;
 import getSnippets from '../utils/get-snippets';
-import elementPage from '../templates/components/element-page';
+import elementPage from '../templates/element-page';
 
 export default {
   name: 'register-elements',
 
   initialize(app) {
-    let snippets = getSnippets();
+    let hasTemplate = (snippet) => app.hasRegistration(
+      `template:components/snippets/${snippet.component}`
+    );
+
+    let snippets = getSnippets().filter(hasTemplate);
     snippets.forEach((snippet) => {
       app.register(`template:${snippet.name}`, elementPage);
       app.register(`controller:${snippet.name}`, Controller.extend({
         snippet
       }));
     });
+
+    app.register('service:snippets', Service.extend({
+      snippets
+    }));
   }
 };
