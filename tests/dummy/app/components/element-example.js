7574959024df164bb6440d586c5f9f3a68ee5897
Dunnkers <dunnkers@hotmail.com>
2017-07-11 15:11:06 +0200

    Conditionally show js in snippet 🚸
    In progress...

--- tests/dummy/app/components/element-example.js
+++ tests/dummy/app/components/element-example.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
+const { computed, inject } = Ember;
 
 export default Ember.Component.extend({
+  snippets: inject.service(),
   tagName: 'paper-material',
-  classNames: [ 'element-example' ]
+  classNames: [ 'element-example' ],
+  hasJS: computed(function() {
+    // debugger;
+    // return this.get('snippets').includes(`${this.get('name')}.js`);
+    return true;
+  })
 });
